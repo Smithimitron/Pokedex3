ff7811f380a0b021b1b16e99d5d3f4193bc51019
Gavin Craft <gavincraft2016@gmail.com>
2018-06-18 09:44:22 -0400

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Gavin Craft on 6/18/18.
+//  Copyright © 2018 Gavin Craft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
